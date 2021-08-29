163b0abc54e9faa1cc2ee68aefc4d951562c5612
timo42453189 <66437994+timo42453189@users.noreply.github.com>
2021-08-29 17:48:05 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,42 +3,43 @@ import threading
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("SERVER_ADDRESS", 55551))
+client.connect(("192.168.178.78", 55553))
 
 def lamp():
 	while True:
 		try:
-			command = client.recv(1024).decode("ascii")
-			if command == "ON":
-				GPIO.setup(18, GPIO.OUT)
-				GPIO.output(18, GPIO.HIGH)
-				print('Successfully set pin 18 up!')
-				time.sleep(3)
-				GPIO.output(18, GPIO.LOW)
-				print('Successfully set pin 18 down!')
-				GPIO.cleanup()
+		    command = client.recv(1024).decode("ascii")
+            print(command)
+		    if command == "ON":
+			GPIO.setup(18, GPIO.OUT)
+			GPIO.output(18, GPIO.HIGH)
+			print('Successfully set pin 18 up!')
+			time.sleep(3)
+			GPIO.output(18, GPIO.LOW)
+			print('Successfully set pin 18 down!')
+			GPIO.cleanup()
 		except:
-			break
+		    break
 
 def user():
 	while True:
 		try:
-			command = input("Command: ")
-			client.send(command.encode("ascii"))
+		    command = input("Command: ")
+		    client.send(command.encode("ascii"))
 		except:
-			break
+		    break
 
 def handle():
 	type = input("User (type in user name) or Lamp (type in LAMP)? ")
 	message = client.recv(1024).decode("ascii")
 	if message == "!USER":
-		client.send(type.encode("ascii"))
-
+	    client.send(type.encode("ascii"))
 	if type == "LAMP":		
-		lamp()
+	    lamp()
 	else:
-		user()
+	    user()
 
 
 
 handle()
+
